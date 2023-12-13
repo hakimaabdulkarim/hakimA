e88a3bca863182687d032f356cca1ccb834c6da1
hakimaabdulkarim <144906884+hakimaabdulkarim@users.noreply.github.com>
2023-12-13 21:03:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+def same_num(num1, num2, num3):
+    result = num1 * num2 * num3
+    print("result is", result)
+
+def sum_def_num1(num1, num2, num3):
+    result = num1 + num2 + num3
+    print("result is", result)
+
+def same_num2(num1, num2, num3):
+    result = (num1 * num2) + num3
+    print("result is", result)
+
+def same_num3(num1, num2, num3):
+    result = (num1 * num3) + num2
+    print("result is", result)
+
+def same_num4(num1, num2, num3):
+    result = (num2 * num3) + num1
+    print("result is", result)
+
+num1 = int(input("enter first num: "))
+num2 = int(input("enter first num: "))
+num3 = int(input("enter first num: "))
+
+
+
+
+
+
+
+
